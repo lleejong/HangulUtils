c865936643e1a4460a7d1cd496619e5f49c0b6e2
lleejong <lleejong@gmail.com>
2017-06-12 16:29:56 +0900

    - 영타 -> 한타 변환 추가

---
+++ src/test/utils/convertEngToKorTest.java
@@ -0,0 +1,14 @@
+package utils;
+
+import org.junit.Test;
+
+/**
+ * Created by lleejong on 2017-06-12.
+ */
+public class convertEngToKorTest {
+    @Test
+    public void convertEngToKorTest(){
+        String output = HangulUtils.convertEngToKor("dkssudgktpdy. dhxkfwkeh wldnjsgksms gksrmf dhxdxhakxkdlqslek.");
+        System.out.println(output);
+    }
+}
